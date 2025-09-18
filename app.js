b581f6e827710f6cf2db558aa83b00c8208f7a3a
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-09-18 23:56:58 +0100

    feat: Intégration de la fonction 'initializeStorage' dans 'init'

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
+import forms from './Modules/forms.js';
 
-function initializeStorage(){
-    if(!localStorage.getItem("Users")){
-        localStorage.setItem("Users", JSON.stringify([]));
-    }
-    if(!localStorage.getItem("Patients")){
-        localStorage.setItem("Patients", JSON.stringify([]));
-    }
-    if(!localStorage.getItem("Recette")){
-        localStorage.setItem("Recette", JSON.stringify([]));
-    }
-    if(!localStorage.getItem("RDV")){
-        localStorage.setItem("RDV", JSON.stringify([]));
-    }
-}
-
-function init(){
-    try{
-        initializeStorage();
-    }catch(error){
-        console.log("Initialized with error : ", error);
+function init() {
+    if (!localStorage.getItem("Users")) localStorage.setItem("Users", JSON.stringify([]));
+    if (!localStorage.getItem("Patients")) localStorage.setItem("Patients", JSON.stringify([]));
+    if (!localStorage.getItem("Recette")) localStorage.setItem("Recette", JSON.stringify([]));
+    if (!localStorage.getItem("RDV")) localStorage.setItem("RDV", JSON.stringify([]));
+    
+    const root = document.getElementById('root');
+    const currentUser = localStorage.getItem("currentUser");
+    
+    if (currentUser) {
+        root.innerHTML = dashboard();
+    } else {
+        root.innerHTML = forms.loginForm();
     }
 }
 
